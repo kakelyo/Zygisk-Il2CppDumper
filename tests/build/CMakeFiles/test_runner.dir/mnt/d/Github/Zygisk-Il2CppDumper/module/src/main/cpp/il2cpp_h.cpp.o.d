0bcf509813db906b7ee7ddde0482b53ac03f0a1f
unknown <qiliang.he@51zjzb.com>
2026-05-28 11:21:46 +0800

    dump all 6

--- tests/build/CMakeFiles/test_runner.dir/mnt/d/Github/Zygisk-Il2CppDumper/module/src/main/cpp/il2cpp_h.cpp.o.d
+++ tests/build/CMakeFiles/test_runner.dir/mnt/d/Github/Zygisk-Il2CppDumper/module/src/main/cpp/il2cpp_h.cpp.o.d
@@ -189,12 +189,5 @@ CMakeFiles/test_runner.dir/mnt/d/Github/Zygisk-Il2CppDumper/module/src/main/cpp/
  /usr/include/c++/13/bits/codecvt.h \
  /usr/include/x86_64-linux-gnu/c++/13/bits/basic_file.h \
  /usr/include/x86_64-linux-gnu/c++/13/bits/c++io.h \
- /usr/include/c++/13/bits/fstream.tcc /usr/include/c++/13/functional \
- /usr/include/c++/13/bits/std_function.h \
- /usr/include/c++/13/unordered_map \
- /usr/include/c++/13/bits/unordered_map.h \
- /usr/include/c++/13/bits/hashtable.h \
- /usr/include/c++/13/bits/hashtable_policy.h \
- /usr/include/c++/13/bits/enable_special_members.h \
- /usr/include/c++/13/array /usr/include/c++/13/sstream \
+ /usr/include/c++/13/bits/fstream.tcc /usr/include/c++/13/sstream \
  /usr/include/c++/13/bits/sstream.tcc
